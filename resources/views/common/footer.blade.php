da18898ea95c7ee3f4017df9b4bca662faabc231
rich <xiegw@xiaolajiao.com>
2017-01-07 15:52:18 +0800

    统计 goolge

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -18,3 +18,14 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="{{ elixir("js/jquery-1.11.1.min.js") }}"></script>
 <script src="{{ elixir("js/search.js") }}"></script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-89956677-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
