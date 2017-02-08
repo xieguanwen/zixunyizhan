85f2eaf751712a7ace9c8f291b30ef4cacb29170
rich <xiegw@xiaolajiao.com>
2017-02-08 15:48:16 +0800

    语言修改和式样

--- resources/lang/zh-cn/common.php
+++ resources/lang/zh-cn/common.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Administrator
+ * User: rich
  * Date: 2016/12/14
  * Time: 15:41
  */
 
 return [
-    'index_keywords'=>'手机,资讯',
-    'index_description'=>'手机资讯',
-    'index_title'=>'手机资讯-首页',
-    '404_title'=>'sorry,你访问不存在。',
-    'search_title'=>'搜索结果',
+    'index_keywords'=>'手机资讯,智能手机,Andriod手机，国产手机、华为手机、魅族手机、小辣椒手机、小米手机、苹果手机、三星手机、酷派手机、一加手机、锤子手机、OPPO手机、VIVO手机',
+    'index_description'=>'资讯驿站是最全的手机资讯中心，提供全面专业的手机资讯、手机测评、行业新闻、手机导购、手机应用解析等信息，涉及国内、外Andriod、YunOS、IOS等系统的苹果、三星、OPPO、VIVO、华为、小辣椒、魅族手机',
+    'index_title'=>'【手机资讯】最新手机咨询大全、拍照手机、Andriod手机-资讯驿站',
+    '404_title'=>'sorry,你访问不存在--咨询驿站',
+    'search_title'=>'搜索结果--资讯驿站',
 ];
